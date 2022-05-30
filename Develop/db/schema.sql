1f09194eb0abd4262d147d7bc69c0b00a6073e45
Kayla Vangel <Kayla.Vangel@comcast.net>
2022-05-30 18:14:17 -0400

    schema

--- Develop/db/schema.sql
+++ Develop/db/schema.sql
@@ -4,3 +4,29 @@ DROP DATABASE IF EXISTS ecommerce_db;
 -- CREATE DATABASE
 CREATE DATABASE ecommerce_db;
 
+CREATE TABLE catergory (
+    id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    category_name STRING NOT NULL
+);
+
+CREATE TABLE product (
+    id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    product_name STRING NOT NULL,
+    price DECIMAL NOT NULL DECIMAL,
+    category_id INT, 
+    FOREIGN KEY (category_id) REFERENCES category (id)
+);
+
+CREATE TABLE tag (
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    tag_name STRING
+);
+
+CREATE TABLE productTag (
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    product_id INT,
+    FOREIGN KEY (product_id) REFERENCES product (id),
+    tag_id INT, 
+    FOREIGN KEY  (tag_id) REFERENCES tag (id)
+);
+
