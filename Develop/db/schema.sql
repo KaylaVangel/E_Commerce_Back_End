8123182fb18072638bb179e17c11cfd1be97865c
Kayla Vangel <Kayla.Vangel@comcast.net>
2022-05-31 18:12:31 -0400

    defined columns for product tag

--- Develop/db/schema.sql
+++ Develop/db/schema.sql
@@ -5,16 +5,3 @@ DROP DATABASE IF EXISTS ecommerce_db;
 CREATE DATABASE ecommerce_db;
 
 
-CREATE TABLE tag (
-    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    tag_name STRING
-);
-
-CREATE TABLE productTag (
-    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    product_id INT,
-    FOREIGN KEY (product_id) REFERENCES product (id),
-    tag_id INT, 
-    FOREIGN KEY (tag_id) REFERENCES tag (id)
-);
-
