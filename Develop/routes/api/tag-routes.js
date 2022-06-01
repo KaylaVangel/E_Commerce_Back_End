f6c15cdef49f788238ae0706f54d330603127234
Kayla Vangel <Kayla.Vangel@comcast.net>
2022-06-01 16:01:39 -0400

    routes doen for tag

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -6,12 +6,49 @@ const { Tag, Product, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all tags
   // be sure to include its associated Product data
+  //check!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+  Tag.findAll =({
+    include: [
+      {
+        model: Product,
+        attributes: ['id', 'product_name', 'price', 'category_id'],
+      },
+    ],
+  })
+    .then(dbTagAll => res.json(dbTagAll))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 
 });
 
 router.get('/:id', (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  //check!!!!!!!!!!!!!!!!
+  Tag.findOne =({
+    where: {
+      id: req.params.id
+    },
+    include: [
+      {
+        model: Product,
+        attributes: ['id', 'product_name', 'price', 'category_id'],
+      },
+    ],
+  })
+  .then(dbTagOne => {
+    if (!dbTagOne) {
+      res.status(404).json({ message: 'No tag found with this id' });
+      return;
+    }
+    res.json(dbTagOne);
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 router.post('/', (req, res) => {
